07f510f89d90d4762d0df702dad75abb7b7ebe3c
Deendayal_dhakad <151893234+Deendayal403@users.noreply.github.com>
2024-07-25 12:53:35 +0530

    Update info.py

--- info.py
+++ info.py
@@ -60,7 +60,7 @@ COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 # Verify
 VERIFY = bool(environ.get('VERIFY', True)) # Verification On ( True ) / Off ( False )
 HOW_TO_VERIFY = environ.get('HOW_TO_VERIFY', 'https://t.me/Mawhextras/6') # How to open tutorial link for verification
-DEENDAYAL_VERIFY_TIME = int(environ.get('DEENDAYAL_VERIFY_TIME', 6)) # Add time in hours
+DEENDAYAL_VERIFY_TIME = int(environ.get('DEENDAYAL_VERIFY_TIME', 4)) # Add time in hours
 
 
 # Others
